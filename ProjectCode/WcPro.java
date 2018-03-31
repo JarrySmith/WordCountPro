51fe04c17bd6345ba7126a0428cd2aa03dc0921c
JarrySmith <853669786@qq.com>
2018-03-31 22:27:02 +0800

    新增了逐步集成各个模块的测试

--- ProjectCode/WcPro.java
+++ ProjectCode/WcPro.java
@@ -1,7 +1,6 @@
 package project;
 import teamwork.*;
-
-
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -20,13 +19,76 @@ public class WcPro {
             String sortedWordFrequency = outputprocess.sort( wordsFrequency);
             outputprocess.outPutToText(sortedWordFrequency);
         }
-        catch (Exception e)
+        catch (IllegalArgumentException e)
         {
             //异常处理
+            e.printStackTrace();
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
         }
         finally {
             //清空环境
         }
 
+    }
+    public void funcpart1(String[]args)
+    {
+        try {
+            InputProcess inputProcess=new InputProcess();
+            String filepath=inputProcess.processInput(args);
+        }
+      catch (IllegalArgumentException e)
+        {
+            //异常处理
+            e.printStackTrace();
+        }
+        finally {
+        //清空环境
+    }
+    }
+    public void funcpart2(String[]args)
+    {
+        try {
+            InputProcess inputProcess=new InputProcess();
+            WordCount wordCount = new WordCount();
+            String filepath=inputProcess.processInput(args);
+            Map<String,Integer> wordsFrequency= wordCount.wordCount(filepath);
+        }
+        catch (IllegalArgumentException e)
+        {
+            //异常处理
+            e.printStackTrace();
+
+        }
+
+        finally {
+            //清空环境
+        }
+    }
+    public void funcpart3(String[]args)
+    {
+        try {
+            InputProcess inputProcess=new InputProcess();
+            Outputprocess outputprocess=new Outputprocess();
+            WordCount wordCount = new WordCount();
+            String filepath=inputProcess.processInput(args);
+            Map<String,Integer> wordsFrequency= wordCount.wordCount(filepath);
+            String sortedWordFrequency = outputprocess.sort( wordsFrequency);
+            outputprocess.outPutToText(sortedWordFrequency);
+        }
+        catch (IllegalArgumentException e)
+        {
+            //异常处理
+            e.printStackTrace();
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            //清空环境
+        }
     }
 }
